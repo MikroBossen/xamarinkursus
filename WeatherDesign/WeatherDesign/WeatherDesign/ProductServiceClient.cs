d286bebdfa6efc70633d472edd06cba48b0a7110
Admin <Admin>
2016-09-14 14:59:45 +0200

    Kursus 14-09-2016

---
+++ WeatherDesign/WeatherDesign/WeatherDesign/ProductServiceClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDesign
+{
+    class ProductServiceClient
+    {
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://firstbossapp.azurewebsites.net/");
+                HttpResponseMessage response = await client.GetAsync("api/products");
+                var products = await response.Content.ReadAsAsync<IEnumerable<Product>>();
+                return products;
+            }
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://firstbossapp.azurewebsites.net/");
+                HttpResponseMessage response = await client.GetAsync("api/products/" + id);
+                var product = await response.Content.ReadAsAsync<Product>();
+                return product;
+            }
+        }
+    }
+}
---
+++ WeatherDesign/WeatherDesign/WeatherDesign/ProductViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDesign
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        private int id;
+        private string name;
+        private string category;
+        private decimal price;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public int Id
+        {
+            get { return this.id; }
+            set
+            {
+                if (value != this.id)
+                {
+                    this.id = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Category
+        {
+            get { return this.category; }
+            set
+            {
+                if (value != this.category)
+                {
+                    this.category = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value != this.price)
+                {
+                    this.price = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
